f2dd3957d793e2f06d719459f51d5f651aaa24b4
gumgu <bienlee@naver.com>
2023-11-15 02:10:00 +0900

    [Refactor] 각 메뉴를 의미하는 클래스 이름 변경 (Order -> Item)

--- src/main/java/christmas/ui/InputView.java
+++ src/main/java/christmas/ui/InputView.java
@@ -1,7 +1,7 @@
 package christmas.ui;
 
 import camp.nextstep.edu.missionutils.Console;
-import christmas.ui.parser.ParseOrder;
+import christmas.ui.parser.ParseItem;
 
 import java.util.List;
 
@@ -14,11 +14,11 @@ public class InputView {
         return input;
     }
 
-    public List<String[]> readOrder() {
+    public List<String[]> readItem() {
         System.out.println("주문하실 메뉴를 메뉴와 개수를 알려 주세요. (e.g. 해산물파스타-2,레드와인-1,초코케이크-1)");
         String input = Console.readLine();
 
-        List<String[]> orders = ParseOrder.parseOrder(input);
-        return orders;
+        List<String[]> items = ParseItem.parseItem(input);
+        return items;
     }
 }
